4e3495139b306712f73697e670be0ac2576714c7
Isaac Turner <spacey_sooty@outlook.com>
2023-09-02 12:13:28 +0800

    changed style

--- src/new_project.rs
+++ src/new_project.rs
@@ -1,6 +1,6 @@
 use clap::{Args, Subcommand};
 use std::fs;
-use std::fs::File;
+use std::process::Command;
 
 #[derive(Args, Debug)]
 pub struct ProjectArgs {
@@ -17,10 +17,20 @@ pub enum Langs {
 }
 
 pub fn rust_project(proj_name: &String) {
+  let git = r#"git init | git add . | git commit -m "Initial Commit""#;
   println!("starting a new rust project, with name {}", proj_name);
+  fs::create_dir(proj_name).unwrap();
+  fs::create_dir(format!("{}/.cargo", proj_name)).unwrap();
+  Command::new("powershell")
+    .arg(format!("cd {}", proj_name))
+    .arg(" | ")
+    .arg("cargo init")
+    .arg(" | ")
+    .arg(git)
+    .spawn()
+    .expect("cargo command failed to start");
 }
 
 pub fn svelte_project(proj_name: &String) {
   println!("starting a new svelte project, with name {}", proj_name);
-  fs::create_dir(&proj_name).unwrap();
 }
