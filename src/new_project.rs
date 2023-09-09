4bf233e2b91d225b8528a20addf33eb1fa5a91d5
Isaac Turner <spacey_sooty@outlook.com>
2023-09-09 12:46:20 +0800

    added cfg directive for different platforms

--- src/new_project.rs
+++ src/new_project.rs
@@ -1,5 +1,6 @@
 use clap::{Args, Subcommand};
 use std::fs;
+use std::io::Write;
 use std::process::Command;
 
 #[derive(Args, Debug)]
@@ -16,21 +17,37 @@ pub enum Langs {
   Svelte,
 }
 
+#[cfg(windows)]
 pub fn rust_project(proj_name: &String) {
   let git = r#"git init | git add . | git commit -m "Initial Commit""#;
+  let confpath = format!("{}/.cargo/config.toml", proj_name);
   println!("starting a new rust project, with name {}", proj_name);
   fs::create_dir(proj_name).unwrap();
   fs::create_dir(format!("{}/.cargo", proj_name)).unwrap();
+  fs::File::create(confpath).unwrap().write_all(b"[profile.release]\nlto=true\nstrip='debuginfo'").unwrap();
   Command::new("powershell")
     .arg(format!("cd {}", proj_name))
     .arg(" | ")
     .arg("cargo init")
+    //.arg(" | ")
+    //.arg("New-Item -Name src/lib.rs -ItemType File")
     .arg(" | ")
     .arg(git)
     .spawn()
     .expect("cargo command failed to start");
 }
 
+#[cfg(windows)]
+pub fn svelte_project(proj_name: &String) {
+  println!("starting a new svelte project, with name {}", proj_name);
+}
+
+#[cfg(not(windows))]
+pub fn rust_project(proj_name: &String) {
+  println!("starting a new rust project, with name {}", proj_name);
+}
+
+#[cfg(not(windows))]
 pub fn svelte_project(proj_name: &String) {
   println!("starting a new svelte project, with name {}", proj_name);
 }
