3b020f059b6217685b7e4dc2b2e6f5eb5d9c2160
Isaac Turner <spacey_sooty@outlook.com>
2023-09-09 18:01:39 +0800

    finished rust v1

--- src/new_project.rs
+++ src/new_project.rs
@@ -24,17 +24,46 @@ pub fn rust_project(proj_name: &String) {
   println!("starting a new rust project, with name {}", proj_name);
   fs::create_dir(proj_name).unwrap();
   fs::create_dir(format!("{}/.cargo", proj_name)).unwrap();
-  fs::File::create(confpath).unwrap().write_all(b"[profile.release]\nlto=true\nstrip='debuginfo'").unwrap();
+  fs::File::create(confpath)
+      .unwrap()
+      .write_all(b"[profile.release]\nlto=true\nstrip='debuginfo'\n")
+      .unwrap();
+  fs::create_dir(format!("{}/src", proj_name)).unwrap();
+  fs::File::create(format!("{}/src/lib.rs", proj_name)).unwrap();
+  fs::File::create(format!("{}/src/main.rs", proj_name))
+      .unwrap()
+      .write_all(b"fn main() {}")
+      .unwrap();
+  fs::File::create(format!("{}/build.rs", proj_name))
+      .unwrap()
+      .write_all(b"fn main() {}")
+      .unwrap();
+  fs::File::create(format!("{}/src/tests.rs", proj_name))
+      .unwrap()
+      .write_all(b"#[cfg(test)]\nmod tests{}\n")
+      .unwrap();
+  fs::File::create(format!("{}/.gitignore", proj_name))
+      .unwrap()
+      .write_all(b"/target\nCargo.lock\n*.exe\n*.out\n")
+      .unwrap();
+  fs::create_dir(format!("{}/.github", proj_name)).unwrap();
+  fs::File::create(format!("{}/.github/dependabot.yml", proj_name))
+      .unwrap()
+      .write_all(b"version: 2\nupdates:\n  - directory: /\n    package-ecosystem: cargo\n    schedule:\n      interval: daily")
+      .unwrap();
+  fs::create_dir(format!("{}/.github/workflows", proj_name)).unwrap();
+  fs::File::create(format!("{}/.github/workflows/ci.yml", proj_name))
+      .unwrap()
+      .write_all(b"name: Build and Test\n\non:\n  push:\n    branches: ['*']\n  pull_request:\n    branches: ['master']\n\njobs:\n  Ubuntu:\n    runs-on: ubuntu-latest\n    steps:\n    - uses: actions/checkout@v3\n    - name: Build\n      run: cargo build --verbose\n    - name: Run Tests\n      run: cargo test --verbose\n\n  Windows:\n    runs-on: windows-latest\n    steps:\n    - uses: actions/checkout@v3\n    - name: Build\n      run: cargo build --verbose\n    - name: Run Tests\n      run: cargo test --verbose")
+      .unwrap();
   Command::new("powershell")
     .arg(format!("cd {}", proj_name))
     .arg(" | ")
     .arg("cargo init")
-    //.arg(" | ")
-    //.arg("New-Item -Name src/lib.rs -ItemType File")
     .arg(" | ")
     .arg(git)
     .spawn()
-    .expect("cargo command failed to start");
+    .expect("powershell command failed to start");
 }
 
 #[cfg(windows)]
