98cf0d0dcfc94c695ec60e5960cdd6cdd82f4afe
Isaac Turner <spacey_sooty@outlook.com>
2023-10-27 12:22:16 +0800

    fmt proper

--- src/main.rs
+++ src/main.rs
@@ -7,33 +7,33 @@ use cli::new_project::ProjectArgs;
 #[command(author = "Isaac Turner")]
 #[command(version = "0.1")]
 #[command(
-  about = "My CLI tool",
-  long_about = "A CLI tool I built to perform tasks I commonly require"
+    about = "My CLI tool",
+    long_about = "A CLI tool I built to perform tasks I commonly require"
 )]
 
 struct Cli {
-  #[command(subcommand)]
-  command: Commands,
+    #[command(subcommand)]
+    command: Commands,
 }
 
 #[derive(Subcommand)]
 enum Commands {
-  New(ProjectArgs),
+    New(ProjectArgs),
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-  println!("Platform: {}", std::env::consts::OS);
+    println!("Platform: {}", std::env::consts::OS);
 
-  let cli = Cli::parse();
-  match &cli.command {
-    Commands::New(project_args) => match &project_args.lang {
-      new_project::Langs::Rust => {
-        new_project::rust_project(&project_args.name, &project_args.origin);
-      }
-      new_project::Langs::Svelte => {
-        new_project::svelte_project(&project_args.name);
-      }
-    },
-  }
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::New(project_args) => match &project_args.lang {
+            new_project::Langs::Rust => {
+                new_project::rust_project(&project_args.name, &project_args.origin);
+            }
+            new_project::Langs::Svelte => {
+                new_project::svelte_project(&project_args.name);
+            }
+        },
+    }
 }
